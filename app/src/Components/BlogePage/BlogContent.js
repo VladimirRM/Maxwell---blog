25b46de7d4480ee7a9810ecbd516f294b23d3336
Vladimir RM <shvdrummer@gmail.com>
2022-11-07 12:21:39 +0200

    export const posts

--- app/src/Components/BlogePage/BlogContent.js
+++ app/src/Components/BlogePage/BlogContent.js
@@ -2,26 +2,7 @@ import React from "react";
 import "./BlogContent.css";
 
 export const BlogContent = () => {
-  const posts = [
-    {
-      id: 1,
-      title: "Post 1",
-      description:
-        " Crime surges and falls for reasons that experts don’t fully understand, and many elected offices have little power to change trends on the ground. ",
-    },
-    {
-      id: 2,
-      title: "Post 1",
-      description:
-        " Crime surges and falls for reasons that experts don’t fully , and many elected offices have little power to change trends on the ground. ",
-    },
-    {
-      id: 3,
-      title: "Post 3",
-      description:
-        " Crime surges and falls for  that experts don’t fully understand, and many elected offices have little power to change trends on the ground. ",
-    },
-  ];
+ 
 
   const blogPosts = posts.map((item) => {
     return (
---
+++ app/src/Shared/projectData.js
@@ -0,0 +1,20 @@
+ export const posts = [
+    {
+      id: 1,
+      title: "Post 1",
+      description:
+        " Crime surges and falls for reasons that experts don’t fully understand, and many elected offices have little power to change trends on the ground. ",
+    },
+    {
+      id: 2,
+      title: "Post 1",
+      description:
+        " Crime surges and falls for reasons that experts don’t fully , and many elected offices have little power to change trends on the ground. ",
+    },
+    {
+      id: 3,
+      title: "Post 3",
+      description:
+        " Crime surges and falls for  that experts don’t fully understand, and many elected offices have little power to change trends on the ground. ",
+    },
+  ];
