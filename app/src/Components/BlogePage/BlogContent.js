49d5062401d066e642bb11817a3ec299b549006a
Vladimir RM <shvdrummer@gmail.com>
2022-11-07 11:32:35 +0200

    add css header

--- app/src/Components/BlogePage/BlogContent.js
+++ app/src/Components/BlogePage/BlogContent.js
@@ -23,41 +23,20 @@ export const BlogContent = () => {
     },
   ];
 
-  const blogPosts = posts.map((item)=>(
-
-  ))
+  const blogPosts = posts.map(() => {
+    return (
+      <div className="post">
+        <h2>{item.title}</h2>
+        <p>{item.description}</p>
+      </div>
+    );
+  });
 
   return (
     <>
       <h1>Simple Blog</h1>
       <div className="posts">
-        <div className="post">
-          <h2>Post 1</h2>
-          <p>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Adipisci
-            commodi optio ipsam ea. Aperiam, velit cupiditate. Sint, est? Error
-            asperiores corrupti alias perspiciatis voluptatibus aliquid
-            delectus, reprehenderit consequuntur debitis provident?
-          </p>
-        </div>
-        <div className="post">
-          <h2>Post 2</h2>
-          <p>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Adipisci
-            commodi optio ipsam ea. Aperiam, velit cupiditate. Sint, est? Error
-            asperiores corrupti alias perspiciatis voluptatibus aliquid
-            delectus, reprehenderit consequuntur debitis provident?
-          </p>
-        </div>
-        <div className="post">
-          <h2>Post 3</h2>
-          <p>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Adipisci
-            commodi optio ipsam ea. Aperiam, velit cupiditate. Sint, est? Error
-            asperiores corrupti alias perspiciatis voluptatibus aliquid
-            delectus, reprehenderit consequuntur debitis provident?
-          </p>
-        </div>
+        {blogPosts}
       </div>
       <div className="count">
         <button>Get amount of posts</button>
