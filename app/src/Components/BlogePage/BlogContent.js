a52560a589d502804d2c156070ab14a3796c7a4a
Vladimir RM <shvdrummer@gmail.com>
2022-11-07 12:19:18 +0200

    add fold shared

--- app/src/Components/BlogePage/BlogContent.js
+++ app/src/Components/BlogePage/BlogContent.js
@@ -16,7 +16,7 @@ export const BlogContent = () => {
         " Crime surges and falls for reasons that experts don’t fully , and many elected offices have little power to change trends on the ground. ",
     },
     {
-      id: 1,
+      id: 3,
       title: "Post 3",
       description:
         " Crime surges and falls for  that experts don’t fully understand, and many elected offices have little power to change trends on the ground. ",
