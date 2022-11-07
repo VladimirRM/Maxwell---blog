af74ad0358b43e48751842d20cd6d227bbdfa41d
Vladimir RM <shvdrummer@gmail.com>
2022-11-07 12:12:12 +0200

    add unique key

--- app/src/Components/BlogePage/BlogContent.js
+++ app/src/Components/BlogePage/BlogContent.js
@@ -25,7 +25,7 @@ export const BlogContent = () => {
 
   const blogPosts = posts.map((item) => {
     return (
-      <div className="post">
+      <div  key={item.id}className="post">
         <h2>{item.title}</h2>
         <p>{item.description}</p>
       </div>
